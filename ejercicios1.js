39fb2d7b3cd6e67cbdc7e18a18c1fd8561345728
wilmerx5 <wilmercampos2004@gmail.com>
2023-04-27 22:19:28 -0500

    ok

--- ejercicios1.js
+++ ejercicios1.js
@@ -6,6 +6,10 @@
 //escribir  una funcion  que recibe  2 numeros, retornar el numeor mayor
 
 
+///nota los operadores logicos sono && significa "y"  y || significa "o"  
+// se usan dentro de condiciones  && con este se deben cumplir todas las condiciones
+// y con este ||  con que una de las dos sea verdadera la condicion va a ser verdadera
+
 //escribir una funcion para determinar si  una perosna es apta para viajar
 // una persona es apta si es mayor de 18 años y menor de 70 años
 
@@ -14,3 +18,7 @@
 
 
 //Escribir una funcion flecha que devuleva "ok" si un  numero es mayor de 50 o si el numero es par 
+
+
+//Escribir una funcion flecha que devuleva "ok" si un  numero es mayor de 50 y si el numero es par 
+
