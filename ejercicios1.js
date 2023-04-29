13b94745ecffe851799ab0388d1346c0b9cfa146
wilmerx5 <wilmercampos2004@gmail.com>
2023-04-28 22:24:32 -0500

    ok

--- ejercicios1.js
+++ ejercicios1.js
@@ -1,6 +1,8 @@
 //Escribir  una variable tipo string, una variable tipu number, una variable, tipo booleano
 // una variable tipo array y una constante de cualquier tipo
 
+const { stringify } = require("git-url-parse")
+
 
 
 //escribir  una funcion  que recibe  2 numeros, retornar el numeor mayor
@@ -21,4 +23,70 @@
 
 
 //Escribir una funcion flecha que devuleva "ok" si un  numero es mayor de 50 y si el numero es par 
+
+
+
+
+
+
+
+
+
+
+///// metodos de listas 
+
+//push agre un elemento  al finla de la lista ej let lista=[0,1,2]
+//  lista.push(3); el resultado sera [0,1,2,3]
+
+///unshift agrega un elemento al principio de la lista  ej: let lista=[0,1,2]
+//  lista.unshift(3); el resultado sera [3,0,1,2]
+
+/// pop elminina el ultimo elemento de la lista ej: let lista=[0,1,2]
+//  lista.pop(); el resultado sera [0,1]
+
+/// shift elminina el primer elemento de la lista ej: let lista=[0,1,2]
+//  lista.shift(); el resultado sera [1,2]
+
+
+/// join junta todos  los elementos de una lsita lo convierte en string  y los pone el separadaor que queramos 
+// ej  let lista=["hola", "mundo"]
+
+// lista.join("-")  el resultado sera "hola, mundo"  
+// (el guion es el separador que le pusimos, puede ser cualquier cosa o simplemente no tener separador)
+
+
+//ejercicio1***********
+// se da una lista de frutas , crear una funcion que reciba como 
+// parametros  la lista y una fruta que se desea añadir
+// rettornar  lista  con todas las frutas
+let frutas=["pera","manzana","durazno"]
+
+
+//ejercicio2***********
+
+// se da una lista de alumnos, agregar un alumno al principio de la lista
+
+let alumnos =["maria","juan","sofia"]
+
+
+//ejercicio3***********
+
+// se da una lista de nombres, elminar el primer  y el ultimo nombre en la lista
+
+let nombres =["maria","juan","sofia","teresa","camilo","karen"]
+
+
+
+//ejercicio4***********
+
+// Se da una lista que en cada posicion tiene una palabra de una frames, converitr esta frace en un string
+// separando cada palabra con un emoji de carita feliz
+
+let frase=["hola","te","amo"]
+
+// console.log(frase.join("@"))
+
+
+
+
 
